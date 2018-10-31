969f1e18ef1e9431492263043d4ba56a033f2101
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2018-10-31 13:57:52 +0100

    Update WorldofZuul/src/worldofzuul/Room.java
    Deleting methods curse and monster

--- WorldofZuul/src/worldofzuul/Room.java
+++ WorldofZuul/src/worldofzuul/Room.java
@@ -31,14 +31,6 @@ public class Room
         return "You are " + description + ".\n" + getExitString();
     }
     
-    public String getMonsterDescription() {
-        return "kicking in the door and is being attacked by a monster. ";
-    }
-    
-    public String getCurseDescription() {
-        return "kicking in the door and is being hit by a curse. ";
-    }
-
     private String getExitString()
     {
         String returnString = "Exits:";

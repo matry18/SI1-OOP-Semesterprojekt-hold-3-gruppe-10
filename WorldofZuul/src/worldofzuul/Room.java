c1029001a8c4d25cd29e0b188a535bc3651aef50
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2018-11-06 15:10:18 +0100

    Update WorldofZuul/src/worldofzuul/Room.java
    Creates a variable of item in Room-class and implements a getter and setter

--- WorldofZuul/src/worldofzuul/Room.java
+++ WorldofZuul/src/worldofzuul/Room.java
@@ -4,72 +4,74 @@ import java.util.Set;
 import java.util.HashMap;
 import java.util.Iterator;
 
+public class Room {
 
-public class Room 
-{
     private String description;
     private HashMap<String, Room> exits;
     private Monster monster;
     private Curse curse;
-    
+    private Item item;
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
     private boolean containsMonster = false;
     private boolean containsCurse = false;
-    
-    public Room(String description) 
-    {
+
+    public Room(String description) {
         this.description = description;
         exits = new HashMap<String, Room>();
     }
 
-    public void setExit(String direction, Room neighbor) 
-    {
+    public void setExit(String direction, Room neighbor) {
         exits.put(direction, neighbor);
     }
 
-    public String getShortDescription()
-    {
+    public String getShortDescription() {
         return description;
     }
 
-    public String getLongDescription()
-    {
-        if(containsMonster){
-        return "You are " + description + ". In here you meet "+ getMonster().getName()+ "\nLevel: " +getMonster().getLevel()+ "\nDescription: "+ getMonster().getDescription()+ "\nBad Stuff: " + getMonster().getBadStuff() + "\n" + getExitString();
-        }
-        else if(containsCurse) {
-        return "You are " + description + ". In this room you are getting hit by a curse "+ getCurse().getName() + "\nDescription: "+getCurse().getDescription()+ "\nBad Stuff: " + getCurse().getBadStuff() + "\n" + getExitString();
-    }
-        else{
-            return "You are " + description +"\n" + getExitString();
+    public String getLongDescription() {
+        if (containsMonster) {
+            return "You are " + description + ". In here you meet " + getMonster().getName() + "\nLevel: " + getMonster().getLevel() + "\nDescription: " + getMonster().getDescription() + "\nBad Stuff: " + getMonster().getBadStuff() + "\n" + getExitString();
+        } else if (containsCurse) {
+            return "You are " + description + ". In this room you are getting hit by a curse " + getCurse().getName() + "\nDescription: " + getCurse().getDescription() + "\nBad Stuff: " + getCurse().getBadStuff() + "\n" + getExitString();
+        } else {
+            return "You are " + description + "\n" + getExitString();
         }
     }
 
-    private String getExitString()
-    {
+    private String getExitString() {
         String returnString = "Exits:";
         Set<String> keys = exits.keySet();
-        for(String exit : keys) {
+        for (String exit : keys) {
             returnString += " " + exit;
         }
         return returnString;
     }
 
-    public Room getExit(String direction) 
-    {
+    public Room getExit(String direction) {
         return exits.get(direction);
     }
 
     public Monster getMonster() {
         return monster;
     }
-public Curse getCurse() {
-    return curse;
-}
+
+    public Curse getCurse() {
+        return curse;
+    }
 
     public void setMonster(Monster monster) {
         this.monster = monster;
         containsMonster = true;
     }
+
     public void setCurse(Curse curse) {
         this.curse = curse;
         containsCurse = true;
@@ -78,6 +80,7 @@ public Curse getCurse() {
     public boolean isContainsMonster() {
         return containsMonster;
     }
+
     public boolean isContainsCurse() {
         return containsCurse;
     }
@@ -85,9 +88,9 @@ public Curse getCurse() {
     public void setContainsMonster(boolean containsMonster) {
         this.containsMonster = containsMonster;
     }
+
     public void setContainsCurse(boolean containsCurse) {
         this.containsCurse = containsCurse;
     }
-    
-}
 
+}

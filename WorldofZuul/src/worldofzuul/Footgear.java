907e4e691e4ace63fc09d057d12bc7e7d6e9ffaa
k3Et <yourk3Et@gmail.com>
2018-11-06 13:53:04 +0100

    Item character (#6)
    * Basics setup to items and characters
    
    Them very simple setup for the two classes items and characters
    
    * Added item-class (incl. subclasses) and updated character-class with inventory ArrayList

---
+++ WorldofZuul/src/worldofzuul/Footgear.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worldofzuul;
+
+/**
+ *
+ * @author Patrick
+ */
+public class Footgear extends Item {
+    
+    public Footgear(String name, int bonus) {
+        super(name, bonus);
+    }
+    
+}

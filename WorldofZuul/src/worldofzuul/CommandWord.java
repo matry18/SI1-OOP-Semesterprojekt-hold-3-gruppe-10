1b1c3756b440ea3f5ce3cba2c72b1bb37a52bcd5
k3Et <yourk3Et@gmail.com>
2018-11-13 14:51:13 +0100

    Removed Roll
    Udkommenterede roll kommandoen

--- WorldofZuul/src/worldofzuul/CommandWord.java
+++ WorldofZuul/src/worldofzuul/CommandWord.java
@@ -1,7 +1,7 @@
 package worldofzuul;
 
 public enum CommandWord {
-    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), FIGHT("fight"), FLEE("flee"), LOOT("loot"), ROLL("roll"), CHARACTER("character");
+    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), FIGHT("fight"), FLEE("flee"), LOOT("loot"), /*ROLL("roll"),*/ CHARACTER("character");
 
     private String commandString;
 

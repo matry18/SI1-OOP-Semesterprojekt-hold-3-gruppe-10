2fa26a81a581e92af97a6818d4a44937609e740e
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2018-11-20 14:43:21 +0100

    Launch to the app
    Starts the GUI app

---
+++ WorldofZuul/src/GUI/GUILaunch.java
@@ -0,0 +1,32 @@
+
+
+package GUI;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUILaunch  extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
+

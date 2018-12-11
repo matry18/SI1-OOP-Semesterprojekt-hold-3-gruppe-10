355ceb47cb7d307babc710d2ee0771d6d719249c
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2018-12-11 14:22:28 +0100

    Window for helping out a player

---
+++ WorldofZuul/src/GUI/AskingForHelpController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author aalsc
+ */
+public class AskingForHelpController implements Initializable {
+    @FXML
+    private Label lblHelpOutPlayer;
+    @FXML
+    private Button btnYes;
+    @FXML
+    private Button btnNo;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Button[] buttons = {btnYes, btnNo};
+        for (Button button: buttons) {
+            setBtnStyle(button);
+        }
+    }    
+
+    @FXML
+    private void handleYesButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleNoButtonAction(ActionEvent event) {
+    }
+ private void setBtnStyle(Button b){
+        b.setStyle("-fx-padding: 5;" + 
+                      "-fx-border-style: solid inside;" + 
+                      "-fx-border-width: 2;" +
+                      "-fx-border-insets: 0;" + 
+                      "-fx-border-radius: 3;" + 
+                      "-fx-border-color: rgb("+86+","+23+","+26+");"+
+                      "-fx-background-color: rgb(" + 223 + ", "+ 196 +", "+169+");");
+    }   
+}
